93bb8aa3a3d89f9c2fece3dca190992ad6fdf919
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-01-02 18:20:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+// pdf task 21 to 25  task 1
+// var a = prompt("Enter your first Name");
+// var b = prompt('Enter your last Name');
+// console.log("Greeting " + a +b)
+
+//pdf task 21 to 25  task 2
+// var a = prompt("Enter your FAvorite model")
+// for(var i =0;i<a.length;i++){
+//     console.log(a)
+// }
+
+
+//pdf task 21 to 25  task 3
+
+// var a = "Pakistani"
+// a=a.indexOf("n")
+// console.log(a)
+
+
+
+//pdf task 21 to 25  task 4
+
+// var b = "Hello World"
+// b=b.lastIndexOf("l")
+// console.log(b)
+
+
+//pdf task 21 to 25  task 5
+
+// var a = "Pakistani"
+// a=a.indexOf("i")
+// console.log(a)
+
+//pdf task 21 to 25  task 6
+
+// var a = prompt("Enter your first Name");
+// var b = prompt('Enter your last Name');
+// var c =  ( a +b)
+// console.log("Greeting "+c)
+
+
+//pdf task 21 to 25  task 7
+
+// var city = "Hyderabad is a cleanist city"
+// var indexNum =city.indexOf('Hydera');
+// var firstChar = city.slice(0,indexNum);
+// var replace="Islama"
+// var third = city.slice(0+6)
+// console.log(firstChar + replace +third)
+
+
+// var text = "Hyderabad is a clean city"
+// var indexText= text.indexOf('Hydera');
+// var firstChar =text.slice(0,indexText)
+// var replace= "Islama"
+// var final = text.slice(0+6)
+// console.log(firstChar + replace +final)
+
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var indextext = message.indexOf("and")
+// var first = message.slice(0,indextext)
+// var replace= "&";
+// var final = message.slice(4+3)
+// console.log(first + replace + final)
+
+// var city = "Hydera bad is a cleanist city"
+// var city1= city.replace("Hydera","Islama")
+// console.log(city1)
+
+// var number = "478"
+// console.log(number)
+
+
+// var str = "pakistani"
+// console.log(str)
+
+// str=str.toLocaleUpperCase()
+// console.log(str)
+
+// var user = prompt("Enter your text")
+// user=user.toLocaleUpperCase()
+// console.log(user)
+
+
+// var  user = prompt("Enter a city Name");
+// user=user.toLowerCase()
+// var arr =["javascript","lahore","islamabad","punjab"]
+// for(var i=0;i<arr.length;i++){
+//     if(arr[i] === user){
+//         alert("City Has Found")
+//     }else{
+//         alert("City  is Not Found")
+//     }break
+// }
+
+
+
+
+// var b= 33.35;
+// var c = Math.ceil(b)
+// console.log(c)
+
+
+// var randonNum = Math.random()*6;
+// console.log(randonNum) 
+
+// var randomNum = Math.random() * 6;
+
+// // console.log(Math.round(randomNum))
+
+// var num = 35.36;
+// var text = String(num);
+// for (let i =0;i<text.length;i++){
+//     if(text.slice(i,i+1)=="."){
+//         text=text.slice(0,i)+ "" + text.slice(i+1)
+//     }
+// }
+
+
+
+
+// var num = 35.36;
+// console.log(text)
+// var text = String(num);
+    
+// for (let i = 0; i < text.length; i++) {
+//     if(text.slice(i,i + 1) == "."){
+//         text = text.slice(0,i) + "" + text.slice(i+1)
+//     }
+// }
+// console.log(text)
